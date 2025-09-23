7d2601390490622d0225573a98300ee93a60b2f1
richujomy <111488257+richujomy@users.noreply.github.com>
2025-09-23 10:59:24 +0530

    task endpoints included

--- fieldops/urls.py
+++ fieldops/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/service-requests/', include('service_requests.urls')),
+    path('api/tasks/', include('tasks.urls')),
     path('api/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
